2a2bbe64132320d65cb556a9b2eaa1765d068142
Raoul <50175388+HariboInc@users.noreply.github.com>
2022-09-27 21:32:45 +0200

    Optimized Language call

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,18 +6,14 @@ description 'ESX Fishing'
 
 client_scripts {
 	'@es_extended/locale.lua',
-	'locales/en.lua',
-	'locales/fr.lua',
-	'locales/nl.lua',
+	'locales/*.lua'
 	'config.lua',
 	'client/main.lua'
 } 
 
 server_scripts {
 	'@es_extended/locale.lua',
-	'locales/en.lua',
-	'locales/fr.lua',
-	'locales/nl.lua',
+	'locales/*.lua'
 	'config.lua',
 	'server/main.lua'
 }

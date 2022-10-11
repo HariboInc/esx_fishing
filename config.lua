a287f2d2199eefc50a753cbc6ae00e33367695bb
Raoul <50175388+HariboInc@users.noreply.github.com>
2022-10-11 20:06:17 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 Config = {}
 
-Config.Locale = 'en'
+Config.Locale = GetConvar('esx:locale', 'en')
 
 Config.CheckForUpdates = true -- Set it to true if you want to get the latest version update info
 
